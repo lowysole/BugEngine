1b93e2149fcda3c0b0fcd1f7d19b78e60c7de312
lowy <lowy.sole@gmail.com>
2020-10-23 12:55:27 +0200

    Add functionalities

--- Vector3/Vec3.cpp
+++ Vector3/Vec3.cpp
@@ -1,24 +1,19 @@
 #include "Vec3.h"
 
-
-Vec3::Vec3() {
-	x = 0.0f;
-	y = 0.0f;
-	z = 0.0f;
-}
-
-Vec3::Vec3(float x, float y, float z) {
-	this->x = x;
-	this->y = y;
-	this->z = z;
-}
-
-Vec3::Vec3(const Vec3& vec3) {
-	x = vec3.x;
-	y = vec3.y;
-	z = vec3.z;
-}
-
-Vec3::~Vec3() {
-
-}
+//Vec3::Vec3() {
+//	x = 0.;
+//	y = 0.;
+//	z = 0.;
+//}
+//
+//Vec3::Vec3(const T &vx, const T &vy, const T &vz) {
+//	x = vx;
+//	y = vy;
+//	z = vz;
+//}
+//
+//Vec3::Vec3(const Vec3 &vec) {
+//	x = vec.x;
+//	y = vec.y;
+//	z = vec.z;
+//}
--- Vector3/main.cpp
+++ Vector3/main.cpp
@@ -1,24 +1,27 @@
 #include "Vec3.h"
 #include <iostream>
+#include <typeinfo>
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-	Vec3* a = new Vec3();
-	Vec3* b = new Vec3(1.0f, 1.0f, 1.0f);
-	Vec3* c = b;
 
-	std::cout << "a --> x=" << a->x << " y= " << a->y << ", z= " << a->z << endl;
-	std::cout << "b --> x=" << b->x << " y= " << b->y << ", z= " << b->z << ;
-	std::cout << "c --> x=" << c->x << " y= " << c->y << " z= " << c->z;
+	Vec3<float> a();
+	Vec3<float> b(1, 2, 3);
+	Vec3<float> c(4,5,6);
+	Vec3<float> d = b + c;
+
+	d.Normalize();
+
+	float distance = d.distance_to(b);
+	float dot_product = d.dot_product(b);
+	Vec3<float> cross_product = d.cross_product(b);
+	float angle = d.angle_between(b);
+
+	//cout << "Distance d-b: " << distance << endl;
+	//cout << "Dot product d-b: " << dot_product << endl;
+	//cout << "Cross product d-b: (" << cross_product.x << ", " << cross_product.y << ", " << cross_product.z << ")" << endl;
+	//cout << "Angle between d-b: " << angle << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
